f4806244c583b2f84a5e845da051b83d2efcd566
dongchul <sorkehdcjf@gmail.com>
2020-07-15 11:54:28 +0900

    feat(Reflection) : add reflection

---
+++ src/main/java/org/example/reflection/App.java
@@ -0,0 +1,40 @@
+package org.example.reflection;
+
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<Book> bookClass = Book.class;
+
+        Book book = new Book();
+        Class<? extends Book> aClass = book.getClass();
+
+        Class<?> aClass1 = Class.forName("org.example.reflection.Book");
+
+        //getFields 는 public 만 가지고 온다.
+        Arrays.stream(bookClass.getFields()).forEach(System.out::println);
+
+        //getDeclaredFields 는 모든 필드에 접근 가능하다.
+        Arrays.stream(bookClass.getDeclaredFields()).forEach(System.out::println);
+
+        Arrays.stream(bookClass.getDeclaredFields())
+                .forEach(f -> {
+                    try {
+                        f.setAccessible(true);
+                        System.out.printf("%s %s", f, f.get(book)) ;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        Arrays.stream(bookClass.getMethods()).forEach(System.out::println);
+
+        Arrays.stream(bookClass.getDeclaredFields())
+                .forEach(f -> {
+                    int modifiers = f.getModifiers();
+                    System.out.println(f);
+                    System.out.println(Modifier.isStatic(modifiers));
+                });
+    }
+}
---
+++ src/main/java/org/example/reflection/MyBook.java
@@ -0,0 +1,4 @@
+package org.example.reflection;
+
+public class MyBook extends Book implements MyInterface{
+}
